7868e74739e8ea498d1d03d2f3b21ebee4a55a88
Vaidotas Zemlys <zemlys@gmail.com>
2013-02-11 07:54:18 +0200

    Add nls2

--- simBB/code.R
+++ simBB/code.R
@@ -1,6 +1,8 @@
 require(midasr)
 
 imidas_r_fast <- midasr:::imidas_r_fast
+ihAh.Td.test <- midasr:::ihAh.Td.test
+ihAh.nls.test <- midasr:::ihAh.nls.test
   
 gendata <- function(n,dk,m,ar,innov.sd,theta) {
    # vv <- simplearma.sim(model=list(ar=ar),(n+10)*m,innov.sd,m)
@@ -53,18 +55,40 @@ size.imr.fast <- function(n,dk,m,ar,innov.sd=1,weight0,cf0,weight1=weight0,cf1=c
     model1 <- na.omit(cbind(y,xmd[,1],V[,1:dk]))
     
     imr.nls <- imidas_r_fast(y,x,dk,weight1,start=cf1+rnorm(length(cf1))/5,imodel="twosteps",model.matrix=model)
+    imr.nls2 <- imidas_r_fast(y,x,dk,weight1,start=cf1+rnorm(length(cf1))/5,imodel="twosteps2",model.matrix=model)
     imr.td <- imidas_r_fast(y,x,dk,weight1,start=cf1+rnorm(length(cf1))/5,imodel="reduced",model.matrix=model1)
     imr.td2 <- imidas_r_fast(y,x,dk,weight1,start=cf1+rnorm(length(cf1))/5,imodel="reduced2",model.matrix=model1)
     
     if(simplify) {
-        inls <- ihAh.nls.test(imr.nls)
-        itd <- ihAh.Td.test(imr.td)
-        itd2 <- ihAh.Td.test(imr.td2)
+        inls <- ihAh.nls.test(imr.nls,"ols")
+        inls2 <- ihAh.nls.test(imr.nls2,"ols")
+        ninls <- ihAh.nls.test(imr.nls,"nls")
+        ninls2 <- ihAh.nls.test(imr.nls2,"nls")
+
+        itd <- ihAh.Td.test(imr.td,"ols")
+        itd2 <- ihAh.Td.test(imr.td2,"ols")
+        nitd <- ihAh.Td.test(imr.td,"nls")
+        nitd2 <- ihAh.Td.test(imr.td2,"nls")
+
         nm <- c("statistic","p.value")
-        return(list(t.nls=inls[nm],t.td=itd[nm],t.td2=itd2[nm],nls=imr.nls$opt,td=imr.td$opt,td2=imr.td2$opt))
+        return(list(se.ols=list(
+                        t.nls=inls[nm],
+                        t.nls2=inls2[nm],
+                        t.td=itd[nm],
+                        t.td2=itd2[nm]),
+                    se.nls=list(
+                        t.nls=ninls[nm],
+                        t.nls2=ninls2[nm],
+                        t.td=nitd[nm],
+                        t.td2=nitd2[nm]
+                        ),
+                    nls=imr.nls$opt,
+                    nls2=imr.nls2$opt,
+                    td=imr.td$opt,
+                    td2=imr.td2$opt))
     }
     else {
-        list(nls=imr.nls,td=imr.td,td2=imr.td2,theta=theta,data=dt,model=model,model1=model1)
+        list(nls=imr.nls,nls2=imr.nls2,td=imr.td,td2=imr.td2,theta=theta,data=dt,model=model,model1=model1)
     }
 }
 
@@ -97,32 +121,41 @@ simtb <- function(N,param,weight0,cf0,weight1=weight0,cf1=cf0,innov.sd=1,simplif
     }
 }
 
-selstat <- function(x,type="td",thresh=0.1,stat="p.value") {
+selstat <- function(x,type="td",thresh=0.1,stat="p.value",se.type="ols") {
     st <- paste("t",type,sep=".")
     if(is.list(x)) {
-	test <- sqrt(sum(x[[type]][["gr0"]]^2))
-	if(test<thresh& x[[type]]$convergence==0) return(x[[st]][[stat]])
+        test <- sqrt(sum(x[[type]][["gr0"]]^2))
+        if(test<thresh& x[[type]]$convergence==0)  {
+            if(se.type %in% names(x)) {
+                return(x[[se.type]][[st]][[stat]])
+            } else {
+                return(x[[st]][[stat]])
+            }
+        }
     }
     NA
 }
-calccol <- function(tb,type="td",thresh=0.1) {
-    lc <- sapply(tb,function(l)sapply(l,selstat,type=type,thresh=thresh))
+
+calccol <- function(tb,type="td",thresh=0.1,se.type="ols") {
+    lc <- sapply(tb,function(l)sapply(l,selstat,type=type,thresh=thresh,se.type=se.type))
     res<-apply(lc,2,function(x){
-	xx<-na.omit(x)
-	c(sum(xx<0.05)/length(xx),length(xx))
+        xx<-na.omit(x)
+        c(sum(xx<0.05)/length(xx),length(xx))
     })
     res<-t(res)
     colnames(res) <- c(type,paste("n",type,sep=""))
     res
 }
-cstarhat<-function(row,alpha=0.05,type="td",thresh=0.1) {
-    t0<-sapply(row,selstat,type=type,thresh=thresh,stat="statistic")
+
+cstarhat<-function(row,alpha=0.05,type="td",thresh=0.1,se.type="ols") {
+    t0<-sapply(row,selstat,type=type,thresh=thresh,stat="statistic",se.type=se.type)
     t0 <- na.omit(t0)
     quantile(t0,1-alpha)
 }
-adjpow <- function(tb,pow,alpha=0.05,type="td",thresh=0.1) {
-    cstar<-sapply(tb,cstarhat,alpha=alpha,type=type,thresh=0.1)
-    lc <- lapply(pow,function(l)sapply(l,selstat,type=type,thresh=thresh,stat="statistic"))
+
+adjpow <- function(tb,pow,alpha=0.05,type="td",thresh=0.1,se.type="ols") {
+    cstar<-sapply(tb,cstarhat,alpha=alpha,type=type,thresh=0.1,se.type=se.type)
+    lc <- lapply(pow,function(l)sapply(l,selstat,type=type,thresh=thresh,stat="statistic",se.type=se.type))
     res <- mapply(function(row,cs){
 	xx<- na.omit(row)
 	c(sum(xx>cs)/length(xx),length(xx))
@@ -132,16 +165,16 @@ adjpow <- function(tb,pow,alpha=0.05,type="td",thresh=0.1) {
     res
 }
 
-sizetable<-function(tbd,param,thresh=0.1) {
-    snls <- calccol(tbd,type="nls",thresh=thresh)
-    std <- calccol(tbd,type="td",thresh=thresh)
-    std2 <- calccol(tbd,type="td2",thresh=thresh)
+sizetable<-function(tbd,param,thresh=0.1,se.type="ols") {
+    snls <- calccol(tbd,type="nls",thresh=thresh,se.type=se.type)
+    std <- calccol(tbd,type="td",thresh=thresh,se.type=se.type)
+    std2 <- calccol(tbd,type="td2",thresh=thresh,se.type=se.type)
     cbind(param,snls,std,std2)
 }
 
-aptable<-function(tbd,pow,param,alpha=0.05,thresh=0.1) {
-    anls<-adjpow(tbd,pow,alpha,type="nls",thresh=thresh)
-    astd <- adjpow(tbd,pow,alpha,type="td",thresh=thresh)
-    astd2<-adjpow(tbd,pow,alpha,type="td2",thresh=thresh)
+aptable<-function(tbd,pow,param,alpha=0.05,thresh=0.1,se.type="ols") {
+    anls<-adjpow(tbd,pow,alpha,type="nls",thresh=thresh,se.type=se.type)
+    astd <- adjpow(tbd,pow,alpha,type="td",thresh=thresh,se.type=se.type)
+    astd2<-adjpow(tbd,pow,alpha,type="td2",thresh=thresh,se.type=se.type)
     cbind(param,anls,astd,astd2)
 }
